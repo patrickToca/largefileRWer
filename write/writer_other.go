7d36421dc87810978a1fe21cd4dadf398c01114c
pmjtoca <pmjtoca@gmail.com>
2026-03-24 10:35:29 +0000

    updates for linux

--- write/writer_other.go
+++ write/writer_other.go
@@ -5,10 +5,13 @@ package write
 import (
 	"log/slog"
 	"os"
+	"runtime"
 )
 
 // enableDirectIO is a no-op on non-Linux platforms
 func (w *Writer) enableDirectIO(file *os.File) error {
-	slog.Debug("Direct I/O not supported on this platform, skipping", "file", file.Name())
+	slog.Debug("Direct I/O not supported on this platform, skipping",
+		"file", file.Name(),
+		"os", runtime.GOOS)
 	return nil
 }
